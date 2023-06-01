f34b757393f32d69c7ed3b1f95012f790511b938
Gervaiskouadio <lelagervais1@gmail.com>
2023-05-31 17:15:40 -0700

    task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_depth - depth of specified node from root
  * @tree: node to check the depth
- * Return: 0 is it is the root or number of depth
+ * Return: 0 it is the root or number of depth
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	retern ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
